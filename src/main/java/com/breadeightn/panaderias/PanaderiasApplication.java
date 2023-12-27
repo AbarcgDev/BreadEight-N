5b3de5d1f657e3fbd284e6feb92eb543a13f8a5e
Alvaro Abarca <abargdev@gmail.com>
2023-12-26 21:03:15 -0600

    plantilla

---
+++ src/main/java/com/breadeightn/panaderias/PanaderiasApplication.java
@@ -0,0 +1,13 @@
+package com.breadeightn.panaderias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanaderiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanaderiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breadeightn/panaderias/PanaderiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breadeightn.panaderias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanaderiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
